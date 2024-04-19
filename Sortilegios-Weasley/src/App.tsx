84b0727250b0920fa725a81cbb003561bbc2617b
savarela18 <savarela.v.18@gmail.com>
2024-04-19 11:09:32 -0500

    Componente de producto actualizado con la calificación y la funcionalidad de la cantidad del producto que se desea añadir al carrito

--- Sortilegios-Weasley/src/App.tsx
+++ Sortilegios-Weasley/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
-import './Bienvenida.css'
-import './Producto.css'
+import './styles/Bienvenida.css'
+import './styles/Producto.css'
 
 import { NavBar } from './components/navBar'
 import { Bienvenida } from './components/Bienvenida'
@@ -13,11 +13,10 @@ function App() {
       <Bienvenida/>
       <h1 style={{color:"white",margin:"1em 2.5em",fontSize:"42px"}}>Productos más vendidos</h1>
       <div className='Productos-mas-vendidos'>
-      <Producto nombre='adas' precio='2' imagen='Producto2'/>
-      <Producto nombre='123' precio='2' imagen='Producto2'/>
-      <Producto nombre='312' precio='2' imagen='Producto2'/>
-      <Producto nombre='312' precio='2' imagen='Producto2'/>
-      <Producto nombre='312' precio='2' imagen='Producto2'/>
+      <Producto nombre='adas' precio='2' imagen='Producto2' calificacion={5}/>
+      <Producto nombre='123' precio='5' imagen='Producto2' calificacion={2}/>
+      <Producto nombre='312' precio='10' imagen='Producto2' calificacion={1}/>
+      <Producto nombre='312' precio='8' imagen='Producto2' calificacion={4}/>
       </div>
     </>
   )
--- Sortilegios-Weasley/src/components/Producto.tsx
+++ Sortilegios-Weasley/src/components/Producto.tsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
+import {Snitch} from "../elements/Snitch"
 
 interface ProductoProps{
     nombre:string;
     precio:string;
     imagen?:string;
+    calificacion:number;
 }
 
-export const Producto:React.FC<ProductoProps>=({nombre, precio,imagen}) => {
+export const Producto:React.FC<ProductoProps>=({nombre, precio,imagen,calificacion}) => {
+    
     const [cantidad,setCantidad] = useState(1)
-    const [calificacion,setCalificacion]=useState(0)
+
+
     const sumar = () =>{
-        setCantidad(cantidad+1)
+        if (cantidad<10){
+            setCantidad(cantidad+1)
+        }else
+        return
     }
     const restar = () =>{
         if (cantidad>1){
@@ -25,26 +32,29 @@ export const Producto:React.FC<ProductoProps>=({nombre, precio,imagen}) => {
         <img className="product-image-card" src={`src/assets/${imagen}.png`} alt="producto" />
         <div className="informacion-producto">
             <h1>{nombre}</h1>
-            <div className="calificacion" style={{display:"flex",flexDirection:"row"}}>
-                <img src="src/assets/snitch.png" alt="" />
-                <img src="src/assets/snitch.png" alt="" />
-                <img src="src/assets/snitch.png" alt="" />
-                <img src="src/assets/snitch.png" alt="" />
-                <img src="src/assets/snitch.png" alt="" />
+            <div className="calificacion">
+                <Snitch calificacion={calificacion}/>
             </div>
-            <p>{precio} Galeones</p>
-            <div className="cantidad-producto">
-                <span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6" onClick={restar} style={{cursor:"pointer"}}>
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                </svg>
+            <div className="cantidad-precio-producto">
+                <span style={{display:"inline-flex", alignItems:"center"}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6" style={{width:"26px"}}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    <p style={{fontSize:"18px"}}>{precio} Galeones</p>
                 </span>
-                <p style={{display:"inline",fontSize:"26px"}}>{cantidad}</p>
-                <span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6" onClick={sumar} style={{cursor:"pointer"}}>
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                <div className="precio-producto" style={{display:"inline-flex", alignItems:"center"}}>
+                <span style={{display:"inline-flex", alignItems:"center"}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6" onClick={restar} style={{cursor:"pointer",width:"24px"}}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                 </svg>
+                <p style={{fontSize:"18px"}}>{cantidad}</p>
                 </span>
+                <span style={{display:"inline-flex", alignItems:"center"}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6" onClick={sumar} style={{cursor:"pointer",width:"24px"}}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                </span>
+                </div>
             </div>
 
             <a href="#">añadir al carrito</a>
